8f6263a15f1ddf0ed7b01d43b82880bd1f82e106
Sonia77-art <soniasobby@gmail.com>
2022-10-19 15:06:33 +0300

    test

---
+++ react-pro/src/App.js
@@ -0,0 +1,24 @@
+
+
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div ClassName="Navbar">
+            <Menu text= "Menu"></Menu>
+            <Menu text= "About"></Menu>
+            <Menu text= "Service"></Menu>
+            <Menu text= "Contact"></Menu>
+        </div>
+      
+      </header>
+      </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ react-pro/src/components/Menu.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+export default function Menu (props) {
+    const [isOpen, setIsOpen] = useState(false);
+    function handleClick(event) {
+        setIsOpen(!isOpen);
+    }
+    return (
+        <div className="Menu">
+            <button onClick={handleClick}>{props.text}</button>
+            { isOpen && <props.children/> }
+        </div>
+    );
+}
